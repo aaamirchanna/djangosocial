1d43bff2077ed56e034ee38f47d7cb0ce88faf07
aamiralichanna <aamirfivr@gmail.com>
2022-07-12 04:21:57 -0700

    2nd commit

--- core/views.py
+++ core/views.py
@@ -10,7 +10,9 @@ from .models import Profile
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
 
 @login_required(login_url='signin')
 def settings(request):
